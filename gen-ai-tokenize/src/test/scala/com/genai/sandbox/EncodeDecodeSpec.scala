2f9d02ef4c1dfa28adff7e3c31b48c19cf4f8a40
joesan <codeintheopen@gmail.com>
2025-02-24 03:42:54 +0100

    'initial commit'

--- gen-ai-tokenize/src/test/scala/com/genai/sandbox/EncodeDecodeSpec.scala
+++ gen-ai-tokenize/src/test/scala/com/genai/sandbox/EncodeDecodeSpec.scala
@@ -9,7 +9,7 @@ import java.nio.charset.StandardCharsets
 
 class EncodeDecodeSpec extends AnyFlatSpec with should.Matchers with BaseSpec {
 
-  val text = "Many common characters, including numerals, punctuation, and other symbols, " +
+  val text: String = "Many common characters, including numerals, punctuation, and other symbols, " +
     "are unified within the standard and are not treated as specific to any given writing system. " +
     "Unicode encodes thousands of emoji, with the continued development thereof conducted by the " +
     "Consortium as a part of the standard.[4] Moreover, the widespread adoption of Unicode was in " +
@@ -21,14 +21,14 @@ class EncodeDecodeSpec extends AnyFlatSpec with should.Matchers with BaseSpec {
       .toSeq.
       map(byte => java.lang.Byte.toUnsignedInt(byte))
 
-  val (_, pairs) = Tokenizer.mergeTokens(20, unsignedValues, maxId)
+  val (_, pairs) = Tokenizer.mergeTokens(2000, unsignedValues, maxId)
   val vocab: Map[Int, Array[Int]] = sandbox.updatedVocab(pairs)
 
   "Encode & Decode" should "encode and decode the input text" in {
     val encoded = Encoder.encode(text, pairs)
-    println(encoded)
-    val decodedText = Decoder(vocab).decode(encoded)
+    println(pairs)
+    val decodedText: String = Decoder(vocab).decode(encoded)
     println(decodedText)
-    assert(encoded == unsignedValues)
+    assert(decodedText == text)
   }
 }
