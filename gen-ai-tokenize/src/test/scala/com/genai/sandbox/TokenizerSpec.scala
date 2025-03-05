bd7d9de56a81491d0f817b6ab5eb39be17151fb1
joesan <codeintheopen@gmail.com>
2025-03-05 07:09:37 +0100

    'initial commit'

--- gen-ai-tokenize/src/test/scala/com/genai/sandbox/TokenizerSpec.scala
+++ gen-ai-tokenize/src/test/scala/com/genai/sandbox/TokenizerSpec.scala
@@ -10,6 +10,15 @@ import org.scalatest.Assertions._
 // https://scalameta.org/munit/docs/getting-started.html
 class TokenizerSpec extends AnyFlatSpec with should.Matchers with BaseSpec {
 
+  "Tokenizer#mergeTokens" should "deal with invalid inputs" in {
+    assertThrows[IllegalArgumentException] {
+      Tokenizer.mergeTokens(ids = Seq.empty, maxId = maxId)
+    }
+    assertThrows[IllegalArgumentException] {
+      Tokenizer.mergeTokens(numMerges = -2, ids = Seq.empty, maxId = maxId)
+    }
+  }
+
   "Tokenizer#getStats" should "fetch the List of recurrent pairs" in {
     assert(ListMap((97,32) -> 2, (32,116) -> 2, (32,97) -> 1, (116,32) -> 1) == Tokenizer.getStats(unsignedValues))
   }
